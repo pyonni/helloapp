52fc1c3cc038628da0b39bfc9a227b6da43da4d1
Полина <polpk2007@gmail.com>
2026-07-07 23:23:42 +0500

    Initial commit

---
+++ hello.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloAppBlack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
